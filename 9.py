be2a2058080f707b899391910594a384cdb678f8
Mario Østervold <162337237+Malarol@users.noreply.github.com>
2025-01-13 14:05:14 +0100

    Helloworldcalc
    Programme treng ikkje hjelp, bere hello world kalkulator

--- 9.py
+++ 9.py
@@ -1,1 +1,13 @@
 print("Dette programmet treng hjelp!")
+print("Kødda prank, du treng dette programmet treng ikkje hjelp")
+print("Velkommen til hello world kalkulator")
+
+try:
+    Tall1 = float(input("Skriv ditt første tall: "))
+    Tegn = input("Kva vil du gjere? +,  -,  *,  /: ")
+    Tall2 = float(input("Skriv ditt andre tall: "))
+
+except ValueError:
+    print("Du må skrive gyldige verdier")
+
+print(f"{Tall1} {Tegn} {Tall2} = Hellow World")
